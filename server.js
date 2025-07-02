d482b6654a2645f93d8fa7c4a6da24769ce36f38
sheikhabd22 <06abdullahshaik@gmail.com>
2025-07-02 20:43:12 +0530

    srever.js changes

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const io = re
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server)
+
+io.on("connection",(socket)=>{
+    console.log("user connected")
+})
 server.listen(8000,()=>{
     console.log("the server is running ")
 })
